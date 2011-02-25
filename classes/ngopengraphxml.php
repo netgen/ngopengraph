beea58d9c2cfef5a40201d623c89ca29e05723b0
Edi Modric <edi.modric@gmail.com>
2011-02-25 13:35:15 +0000

    Added XML datatype handler

---
+++ classes/ngopengraphxml.php
@@ -0,0 +1,16 @@
+<?php
+
+class ngOpenGraphXml extends ngOpenGraphBase
+{
+	function __construct( eZContentObjectAttribute $attribute )
+	{
+		parent::__construct( $attribute );
+	}
+
+	public function getData()
+	{
+		return str_replace("\n", " ", strip_tags(trim($this->ContentObjectAttribute->attribute('data_text'))));
+	}
+}
+
+?>
